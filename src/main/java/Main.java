c8e158b7380c40cb41142ad642cd468cbcf472af
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-02-12 12:30:25 +0300

    Change Initial commit

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BonusService service = new BonusService();
-
-        long bonus = service.calculate(10000, true);
-        System.out.println(bonus);
-    }
-}
